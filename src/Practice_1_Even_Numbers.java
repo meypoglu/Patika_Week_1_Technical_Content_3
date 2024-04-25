c3adfaee534f8fcdec36b420ece53c1b22cc4800
meyup <m.eypoglu@gmail.com>
2024-04-25 16:56:38 +0300

    added comments

--- src/Practice_1_Even_Numbers.java
+++ src/Practice_1_Even_Numbers.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Practice_1_Even_Numbers {
     public static void main(String[] args) {
+        // Kullanıcıdan girdi alınır
         Scanner input = new Scanner(System.in);
         System.out.print("Bir sayı giriniz: ");
         int num = input.nextInt();
+        // Döngüde girilen sayıya kadar i değerini artırarak ilerler
         for (int i = 0;i < num;i++) {
+            // Sayının 2'ye bölümünden kalan 0 ise ekrana verilir
             System.out.print((i % 2 == 0) ? i + "\n": "");
         }
     }
