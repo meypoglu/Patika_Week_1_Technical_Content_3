35dc75ab377597bd188e558ae0cb99f2430ada63
meyup <m.eypoglu@gmail.com>
2024-04-26 16:34:37 +0300

    update

--- src/Practice_10_EBOB_EKOK.java
+++ src/Practice_10_EBOB_EKOK.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 
 public class Practice_10_EBOB_EKOK {
     public static void main(String[] args) {
+        // Kullanıcıdan girdilerin alınması
         Scanner input = new Scanner(System.in);
         System.out.println("Birinci sayıyı giriniz: ");
         int num1 = input.nextInt();
         System.out.println("İkinci sayıyı giriniz: ");
         int num2 = input.nextInt();
 
+        // Değişkenlerin tanımlanması
         int i = 1;
         int k = 1;
         int ebob = 0;
         int ekok = 0;
+        // Eğer iki sayı da aynı bölene bölünüyorsa ebob olarak isimlendirilir, döngü devam ettikçe bu değer değişir ve en son alınan değer gerçek EBOB değeridir
         while (i < num1) {
             i++;
             ebob = num1 % i == 0 && num2 % i == 0 ? i : ebob;
         }
+        // EKOK hesaplama formülü
         ekok = (num1 * num2 ) / ebob;
         System.out.println("EBOB: " + ebob);
         System.out.println("EKOK: " + ekok);
--- src/Practice_11_Find_Lowest_Among_Numbers.java
+++ src/Practice_11_Find_Lowest_Among_Numbers.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Practice_11_Find_Lowest_Among_Numbers {
     public static void main(String[] args) {
+        // Kullanıcıdan girdilerin alınması
         Scanner input = new Scanner(System.in);
         System.out.println("Kaç adet sayı gireceksiniz ?: ");
         int inpCount = input.nextInt();
         System.out.println("1. sayıyı giriniz: ");
         int inpNum = input.nextInt();
+        // En düşük ve yüksek sayıları depolayan değişkenler
         int bankLow = inpNum;
         int bankHigh = 0;
+        // İstenen sayıda döngü çalışıp sayıları alır, bir önceki sayıdan büyük ya da küçük olma durumuna göre ilgili değişkenlere değerler girilir
         for (int i = 0;i < inpCount -1;i++) {
             System.out.println(((i + 2) + ". sayıyı giriniz: "));
             inpNum = input.nextInt();
