8db4a89635a6194e471b7477364eb0ca52267d9b
meyup <m.eypoglu@gmail.com>
2024-04-30 16:00:58 +0300

    update

---
+++ src/Practice_15_Fibonacci_Series.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Practice_15_Fibonacci_Series {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Eleman sayısını giriniz: ");
+        int num = input.nextInt();
+
+        int prev = 1;
+        int next = 2;
+        System.out.print(1 + " " + 1 + " " + 2 + " ");
+        for (int counter = 2;counter < num;counter++) {
+            next += prev;
+            prev = next - prev;
+            System.out.print(next + " ");
+        }
+
+    }
+}
