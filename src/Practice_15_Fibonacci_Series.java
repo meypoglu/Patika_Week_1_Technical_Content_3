ce248b5c479f819dfa2d0c7c5af55e11349286de
meyup <m.eypoglu@gmail.com>
2024-04-30 16:01:47 +0300

    update

--- src/Practice_15_Fibonacci_Series.java
+++ src/Practice_15_Fibonacci_Series.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Practice_15_Fibonacci_Series {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Eleman sayısını giriniz: ");
+        System.out.print("Eleman sayısını giriniz: ");
         int num = input.nextInt();
 
         int prev = 1;
         int next = 2;
         System.out.print(1 + " " + 1 + " " + 2 + " ");
-        for (int counter = 2;counter < num;counter++) {
+        for (int counter = 3;counter < num;counter++) {
             next += prev;
             prev = next - prev;
             System.out.print(next + " ");
